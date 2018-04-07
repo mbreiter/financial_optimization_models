b4e901762f2af073266b8c8a9e6da3743592b0a5
DankK97 <kecmand97@yahoo.com>
2018-04-07 12:42:12 -0400

    Update diverse_mvo.m

--- diverse_mvo.m
+++ diverse_mvo.m
@@ -1,7 +1,14 @@
-function [ output_args ] = diverse_mvo( input_args )
+function x_optimal = diverse_mvo(mu, Q, targetRet, card)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
+n = size(Q,1);
+rho = corrcov(Q);
 
+clear model;
+model.Q = [];
+model.obj = cat(1, rho(:),zeros(n,1));
+size_constraint = cat(2, zeros(1, pow(n,2)), ones(1,n));
+one_rep_per_asset = blkdiag(
 
 end
 
